c43408666a0636e2ac8e18d69a38550317b916a8
anurajrawat <anurajsingh019@gmail.com>
2021-11-12 23:56:57 +0800

    Delete Landing Page directory

--- Landing Page/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() => {
-  // As user scrolls down, reduce opacity of scroll caret
-  $(window).scroll(function() {
-    $("#caret").css("opacity", 1 - $(window).scrollTop() / 500);
-  });
-});
